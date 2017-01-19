028a6c0c20c4fe68b59c5174f517cbf9ff875b16
simakova <simakova.kv@gmail.com>
2017-01-19 16:19:55 +0300

    add LDB classes

---
+++ WpfParser/WpfParser/LDBClasses/Word.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace WpfParser.Model
+{
+    public class Word
+    {
+        [Key]
+        // public int Id { get; set; }
+        public string Wnid { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public string Description { get; set; }
+        public string Count { get; set; }
+        public string Popularity { get; set; }
+        //public List<Hyponim> Hyponims { get; set; }
+    }
+}
