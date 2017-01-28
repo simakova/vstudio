f7b764a21b6282a9ae5146d168cd4ec2e7663feb
simakova <simakova.kv@gmail.com>
2017-01-28 16:20:58 +0300

    add Web Api

---
+++ WebApi/WebService/WebClient/WebForm1.aspx
@@ -0,0 +1,48 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebClient.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    <table>
+            <tr>
+                <td>WNID</td>
+                <td><asp:TextBox ID="txtWnid" runat="server"></asp:TextBox></td>
+            </tr>
+            <tr>
+                <td>Name</td>
+                <td><asp:TextBox ID="txtName" runat="server"></asp:TextBox></td>
+            </tr>
+            <tr>
+                <td>Category</td>
+                <td><asp:TextBox ID="txtCategory" runat="server"></asp:TextBox></td>
+            </tr>
+            <tr>
+                <td>Description</td>
+                <td><asp:TextBox ID="txtDescription" runat="server"></asp:TextBox></td>
+            </tr>
+            <tr>
+                <td>Count</td>
+                <td><asp:TextBox ID="txtCount" runat="server"></asp:TextBox></td>
+            </tr>
+            <tr>
+                <td>Popularity</td>
+                <td><asp:TextBox ID="txtPopularity" runat="server"></asp:TextBox></td>
+            </tr>
+        </table>
+       <table>
+            <tr>
+                <td><asp:Button ID="btnClear" runat="server" Text="Clear" OnClick="btnClear_Click" /></td>
+                <td><asp:Button ID="btnFindWord" runat="server" Text="Find" OnClick="btnFindWord_Click" /></td>
+           </tr>
+        </table>
+    </div>
+    </form>
+</body>
+</html>
---
+++ WebApi/WebService/WebClient/WebForm1.aspx.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using WebClient.ImageService;
+
+namespace WebClient
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnClear_Click(object sender, EventArgs e)
+        {
+            txtWnid.Text = "";
+            txtName.Text = "";
+            txtCategory.Text = "";
+            txtDescription.Text = "";
+            txtCount.Text = "";
+            txtPopularity.Text = "";
+
+        }
+
+        protected void btnFindWord_Click(object sender, EventArgs e)
+        {
+           
+            var client = new ImageWebServiceSoapClient();
+            if (txtWnid.Text.Length != 0)
+            {
+                var wnid = txtWnid.Text;
+                var findName = client.FindWord(wnid, "fromId");
+                txtName.Text = findName.Name;
+                txtCategory.Text = findName.Category;
+                txtDescription.Text = findName.Description;
+                txtCount.Text = findName.Count;
+                txtPopularity.Text = findName.Popularity;
+            }
+            else if (txtName.Text.Length != 0)
+            {
+                var name = txtName.Text;
+                var findName = client.FindWord(name, "fromWord");
+                txtWnid.Text = findName.Wnid;
+                txtCategory.Text = findName.Category;
+                txtDescription.Text = findName.Description;
+                txtCount.Text = findName.Count;
+                txtPopularity.Text = findName.Popularity;
+            }
+       }
+    }
+}
